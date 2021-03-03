1a5bba5c4f5b93b806a7cf7cd2a51a984460d1fa
snomiao <snomiao@gmail.com>
2021-03-04 04:46:43 +0800

    add workflow

--- packages/sno-mongo-ku/README.md
+++ packages/sno-mongo-ku/README.md
@@ -2,23 +2,71 @@
 
 a wrapper to mongodb driver, provide simple method in chinese to handle the mongodb.
 
-Have fun~
+Just have fun with chinese programming ~
+
+## Usage
+
+### 1. Insert and print
+
+These examples have the same purpose:
+1. clean db
+2. insert a log
+3. print it out.
 
 ```typescript
-export async function 本地日志测试() {
-    const 本地库 = snoMongoKu('mongodb://localhost:27017/');
-    (async () => {
-        const 日志 = 本地库('测试')('日志')
-        await 日志.增({ 消息: `连接成功`, 刻: +new Date() })
-        console.log(await 日志.查列({}))
-    })().finally(() => 本地库.断开())
-    
-    /** avaliable functions
-     增, 增补, 增列, 增补列,
-     删,
-     查, 查找, 查数, 查列,
-     改, 聚合,
-     映射,
-     */
-}
+import snoMongoKu from 'sno-mongo-ku'
+; (async()=>{
+    const db = await snoMongoKu('mongodb://localhost:27017/test-snomongoku')
+    await db.logs.drop()
+    await db.logs.insertOne({ createdAt: new Date(), content: 'test/(20210304)' })
+    console.table( await db.logs.find().toArray() )
+    // await db._client.close() // optional
+})()
 ```
+
+```typescript
+import snoMongoKu from 'sno-mongo-ku'
+; (async()=>{
+    const db = await snoMongoKu('mongodb://localhost:27017/测试-雪芒果库')
+    await db.日志.销毁()
+    await db.日志.单增({ 创建于: new Date(), 内容: '测试/(20210304)' })
+    console.table( await db.日志.多查列() )
+    // await db._client.close() // optional
+})()
+```
+
+### 2. upsert
+
+```typescript
+import snoMongoKu from 'sno-mongo-ku'
+; (async()=>{
+    const db = await snoMongoKu('mongodb://localhost:27017/test-snomongoku')
+    await db.logs.drop()
+    await db.logs.upsertOne({_id: 'test', createdAt: new Date(), content: 'test/(20210304)' })
+    console.table( await db.logs.find().toArray() )
+    // await db._client.close() // optional
+})()
+```
+
+```typescript
+import snoMongoKu from 'sno-mongo-ku'
+; (async()=>{
+    const db = await snoMongoKu('mongodb://localhost:27017/测试-雪芒果库')
+    await db.日志.销毁()
+    await db.日志.单增改({_id: 'test'   创建于: new Date(), 内容: '测试/(20210304)' })
+    console.table( await db.日志.多查列() )
+    // await db._client.close() // optional
+})()
+```
+
+### 3. More...
+
+The `db._` you have got is just a alias of `client.db.collection($)`, which supports all MongoDB driver's collection methods, feel free to play with it.
+
+For further usages, you can simply type `db._` and press `ctrl + space` on vscode to view how to use them on below.
+
+Methods: `单增 单删 单改 单查 单查替 单查改 单查删 单补 单增改 upsertOne 多增 多删 多改 多查 多查数 多查列 多补 多增改 upsertMany 聚合 索引 索引删 复索引 复索引删 状态 数量估计 监视 改名 名称 去重 销毁`
+
+## ref
+
+- [自动化发布npm包及生成Github Changelog]( https://banyudu.com/posts/auto_publish_npm_and_generate_github_changelog.882513 )
---
+++ packages/sno-mongo-ku/src/snoMongoKu.ts
@@ -0,0 +1,60 @@
+/**
+ * MongoDB simple wrapper, Provide simple method in chinese to handle the mongodb.
+ * author: YiDong Zhuo(snomiao@gmail.com)
+ */
+
+import mongodb from 'mongodb'
+
+// unit test
+if (require.main === module) (async () => {
+    const db = await snoMongoKu('mongodb://localhost:27017/测试')
+    await db.日志.单增({ 创建于: new Date(), 内容: '测试/(20210304)' })
+    await db.日志.单补({ _id: 'test', 创建于: new Date(), 内容: '测试/(20210304)' })
+    console.table(await db.日志.多查列({}))
+    await db._client.close()
+})().then(console.log).catch(console.error)
+
+type 表 = { _id?: mongodb.ObjectID | any, [x: string]: any; }
+export const 合集增强 = (合集: mongodb.Collection) => Object.assign(合集, {
+    单增: 合集.insertOne,
+    单删: 合集.deleteOne,
+    单改: 合集.updateOne,
+    单查: 合集.findOne,
+    单查替: 合集.findOneAndReplace,
+    单查改: 合集.findOneAndUpdate,
+    单查删: 合集.findOneAndDelete,
+    单补: (表: 表, 索引键列: string[] = ['_id'], 选项?: mongodb.UpdateOneOptions) => 合集.updateOne(Object.fromEntries(索引键列.map(键 => [键, 表[键]])), { $set: 表 }, { upsert: true, ...选项 }),
+    单增改: ((查询: any, 更新: any, options: any, cb?: any) => 合集.updateOne(查询, 更新, { upsert: true, ...options }, cb)) as typeof 合集.updateOne,
+    upsertOne: ((查询: any, 更新: any, options: any, cb?: any) => 合集.updateOne(查询, 更新, { upsert: true, ...options }, cb)) as typeof 合集.updateOne,
+    多增: 合集.insertMany,
+    多删: 合集.deleteMany,
+    多改: 合集.updateMany,
+    多查: 合集.find,
+    多查数: (查询表: mongodb.FilterQuery<any> = {}, 选项?: mongodb.FindOneOptions<any>) => 合集.find(查询表, 选项).count(),
+    多查列: (查询表: mongodb.FilterQuery<any> = {}, 选项?: mongodb.FindOneOptions<any>) => 合集.find(查询表, 选项).toArray(),
+    多补: (表列: 表[], 索引键列: string[] = ['_id'], 选项?: mongodb.CollectionBulkWriteOptions) => 合集.bulkWrite(表列.map((表: 表) => ({ updateOne: { filter: Object.fromEntries(索引键列.map(键 => [键, 表[键]])), update: { $set: 表 }, upsert: true } })), 选项),
+    多增改: ((查询: any, 更新: any, options: any, cb?: any) => 合集.updateMany(查询, 更新, { upsert: true, ...options }, cb)) as typeof 合集.updateMany,
+    upsertMany: ((查询: any, 更新: any, options: any, cb?: any) => 合集.updateMany(查询, 更新, { upsert: true, ...options }, cb)) as typeof 合集.updateMany,
+    聚合: 合集.aggregate,
+    索引: 合集.createIndex,
+    索引删: 合集.dropIndex,
+    复索引: 合集.createIndexes,
+    复索引删: 合集.dropIndexes,
+    状态: 合集.stats,
+    数量估计: 合集.estimatedDocumentCount,
+    监视: 合集.watch,
+    改名: 合集.rename,
+    名称: 合集.collectionName,
+    去重: 合集.distinct,
+    销毁: 合集.drop,
+})
+// ref https://zhuanlan.zhihu.com/p/59434318
+const 返回值类型获取 = <T>(_需推断函数: (_: any) => T): T => ({} as T)
+const 合集增强虚拟返回值 = 返回值类型获取(合集增强);
+type 增强合集 = typeof 合集增强虚拟返回值;
+
+export default async function snoMongoKu(uri: string) {
+    const client = await mongodb.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    return new Proxy(client.db(), { get: (t, p) => p === '_client' ? client : t[p] ?? 合集增强(t.collection(p.toString())) }
+    ) as (mongodb.Db & { _client: mongodb.MongoClient, _demoCollection: 增强合集, _示例合集: 增强合集 } & { [k: string]: 增强合集 })
+}
