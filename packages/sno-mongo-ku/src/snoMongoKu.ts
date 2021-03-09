e9a27873edea2a67e1b5e7928838f9a379ccf297
snomiao <snomiao@gmail.com>
2021-03-09 19:07:09 +0800

    fix: doc._id is required

--- packages/sno-mongo-ku/src/snoMongoKu.ts
+++ packages/sno-mongo-ku/src/snoMongoKu.ts
@@ -79,6 +79,7 @@ const 合集增强 = (合集: mongodb.Collection) => Object.assign(合集, {
             $project && { $project }, //出于索引性能的原因 project要放在sort之后
             $limit && { $limit },
         ].filter(e => e))) {
+            if(!doc._id) throw new TypeError('doc._id is required')
             await q.onEmpty(); q.add(async () => {
                 const UpdateQuery = await func(doc, index, count)
                 UpdateQuery && await 合集.updateOne({ _id: doc._id }, UpdateQuery)
