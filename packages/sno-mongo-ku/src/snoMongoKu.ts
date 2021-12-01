213597f2d90150c9df48b1ff8964040e81065c12
snomiao <snomiao@gmail.com>
2021-12-02 02:13:28 +0800

    feat: 早于

--- packages/sno-mongo-ku/src/snoMongoKu.ts
+++ packages/sno-mongo-ku/src/snoMongoKu.ts
@@ -327,3 +327,39 @@ const _合集增强表 = (合集: mongodb.Collection) => ({
 function 合集增强(合集: mongodb.Collection) {
     return Object.assign(合集, _合集增强表(合集));
 }
+
+
+export const 不晚于 = (乘数: number) => ({
+    秒前: ({ $not: { $gt: new Date(+new Date().getTime() - 1e3 * 乘数) } }),
+    分钟前: ({ $not: { $gt: new Date(+new Date().getTime() - 60e3 * 乘数) } }),
+    小时前: ({ $not: { $gt: new Date(+new Date().getTime() - 3600e3 * 乘数) } }),
+    天前: ({ $not: { $gt: new Date(+new Date().getTime() - 86400e3 * 乘数) } }),
+    月前: ({ $not: { $gt: new Date(+new Date().getTime() - 30 * 86400e3 * 乘数) } }),
+    年前: ({ $not: { $gt: new Date(+new Date().getTime() - 365 * 86400e3 * 乘数) } }),
+})
+
+export const 不早于 = (乘数: number) => ({
+    秒前: ({ $not: { $lt: new Date(+new Date().getTime() - 1e3 * 乘数) } }),
+    分钟前: ({ $not: { $lt: new Date(+new Date().getTime() - 60e3 * 乘数) } }),
+    小时前: ({ $not: { $lt: new Date(+new Date().getTime() - 3600e3 * 乘数) } }),
+    天前: ({ $not: { $lt: new Date(+new Date().getTime() - 86400e3 * 乘数) } }),
+    月前: ({ $not: { $lt: new Date(+new Date().getTime() - 30 * 86400e3 * 乘数) } }),
+    年前: ({ $not: { $lt: new Date(+new Date().getTime() - 365 * 86400e3 * 乘数) } }),
+})
+
+export const 晚于 = (乘数: number) => ({
+    秒前: ({ $gt: new Date(+new Date().getTime() - 1e3 * 乘数) }),
+    分钟前: ({ $gt: new Date(+new Date().getTime() - 60e3 * 乘数) }),
+    小时前: ({ $gt: new Date(+new Date().getTime() - 3600e3 * 乘数) }),
+    天前: ({ $gt: new Date(+new Date().getTime() - 86400e3 * 乘数) }),
+    月前: ({ $gt: new Date(+new Date().getTime() - 30 * 86400e3 * 乘数) }),
+    年前: ({ $gt: new Date(+new Date().getTime() - 365 * 86400e3 * 乘数) }),
+})
+export const 早于 = (乘数: number) => ({
+    秒前: ({ $lt: new Date(+new Date().getTime() - 1e3 * 乘数) }),
+    分钟前: ({ $lt: new Date(+new Date().getTime() - 60e3 * 乘数) }),
+    小时前: ({ $lt: new Date(+new Date().getTime() - 3600e3 * 乘数) }),
+    天前: ({ $lt: new Date(+new Date().getTime() - 86400e3 * 乘数) }),
+    月前: ({ $lt: new Date(+new Date().getTime() - 30 * 86400e3 * 乘数) }),
+    年前: ({ $lt: new Date(+new Date().getTime() - 365 * 86400e3 * 乘数) }),
+})
